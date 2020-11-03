6bc9b1cf5b79ce688c7b0ba3cf5ccd68e4af475c
Alexis Delgado <dalexisbr@gmail.com>
2020-11-03 11:26:12 -0700

    Splitting the App Into Components

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+import GoalInput from './components/GoalInput';
+import GoalItem from './components/GoalItem'
 
 export default function App() {
+  
+  const [courseGoals, setCourseGoals] = useState([])
+
+  const addGoalHandler = (goal) => {
+    setCourseGoals( currentState => [...currentState, {
+      id: Math.random().toString(),
+      value: goal
+    }])
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <GoalInput addGoalHandler={addGoalHandler} />
+      <FlatList 
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={ itemData => <GoalItem onDelete={() => console.log('Does that work?')} title={itemData.item.value} />}
+      /> 
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  screen: {
+    padding: 50
+  }
+})
---
+++ components/GoalItem.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { View, Text, StyleSheet, TouchableNativeFeedback } from 'react-native'
+
+const GoalItem = props => {
+  return (
+    <TouchableNativeFeedback onPress={props.onDelete}>
+      <View style={styles.listItem}>
+        <Text>{props.title}</Text>
+      </View>
+    </TouchableNativeFeedback>
+  ) 
+}
+
+const styles = StyleSheet.create({
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderWidth: 1
+  }
+})
+
+export default GoalItem
